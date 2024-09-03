817a7932956faa98f4753eacc9528947b80ffa9d
PH2S <phsantosdasilva03@outlook.com>
2024-09-03 01:16:52 -0300

    Atividade Finalizada

--- Atividade-2/Exerc10/exerc10.php
+++ Atividade-2/Exerc10/exerc10.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Atividade 6</title>
+    <title>Atividade 10</title>
 </head>
 <body>
-<h1>Atividade 06</h1>
+<h1>Atividade 10</h1>
     <div class="container mt-5">
-        <h2>Incrementando</h2>
+        <h2>Tabuada</h2>
         <form method="POST">
             <div class="mb-3">
                 <label for="num1" class="form-label">Informe o número:</label>
@@ -23,10 +23,13 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             try {
                 $num1 = $_POST['num1'];
-                for ($i= 1; $i <= $num1; $i++)
+                $tabuada = 0;
+                for ($i = 0; $i <= 10; $i++)
                 {
-                    echo "<div class='mt-3 alert alert-success'>Resultado: " . $i . "</div>";
+                    $tabuada = $num1 * $i;
+                    echo "<div class='mt-3 alert alert-success'>Resultado: " . $num1 . " x " . $i . " = " . $tabuada . "</div>";
                 }
+                
                 
             } catch (Exception $e) {
                 echo "<div class='mt-3 alert alert-danger'>Erro: " . $e->getMessage() . "</div>";
--- Atividade-2/Exerc8/exerc8.php
+++ Atividade-2/Exerc8/exerc8.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Atividade 6</title>
+    <title>Atividade 8</title>
 </head>
 <body>
-<h1>Atividade 06</h1>
+<h1>Atividade 08</h1>
     <div class="container mt-5">
-        <h2>Incrementando</h2>
+        <h2>Decrementando</h2>
         <form method="POST">
             <div class="mb-3">
                 <label for="num1" class="form-label">Informe o número:</label>
@@ -23,10 +23,15 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             try {
                 $num1 = $_POST['num1'];
-                for ($i= 1; $i <= $num1; $i++)
-                {
-                    echo "<div class='mt-3 alert alert-success'>Resultado: " . $i . "</div>";
+                if ($num1 <= 0) {
+                    echo "Por favor, insira um número maior que 0.";
                 }
+
+                do {
+                    echo "<div class='mt-3 alert alert-success'>Resultado: " . $num1 . "</div>";
+                    $num1--;
+                } while ($num1 >= 1);
+                
                 
             } catch (Exception $e) {
                 echo "<div class='mt-3 alert alert-danger'>Erro: " . $e->getMessage() . "</div>";
